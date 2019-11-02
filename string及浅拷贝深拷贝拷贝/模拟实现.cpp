388ff60240f6c0e7938f2fa7c14045e3fe094de9
sonissa <48381669+sonissa@users.noreply.github.com>
2019-11-02 22:24:27 +0800

    模拟实现

--- string及浅拷贝深拷贝拷贝/模拟实现.cpp
+++ string及浅拷贝深拷贝拷贝/模拟实现.cpp
@@ -1,34 +1,34 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-//基础模拟实现
-#include<iostream>
-#include<assert.h>
-using namespace std;
-
-namespace self{
-	class string{
-	public:
-		string(const char* s = " "){
-			if (s == nullptr){
-				assert(false);
-				return;
-			}
-			_s = new char[strlen(s) + 1];
-			strcpy(_s, s);
-		}
-		~string(){
-			if (_s){
-				delete[] _s;
-				_s = nullptr;
-			}
-		}
-	private:
-		char* _s;
-	};
-}
-int main(){
-	self::string k = "hello";
-	self::string i("world");
-	self::string m;
-	//self::string l(k);
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
+////基础模拟实现
+//#include<iostream>
+//#include<assert.h>
+//using namespace std;
+//
+//namespace self{
+//	class string{
+//	public:
+//		string(const char* s = " "){
+//			if (s == nullptr){
+//				assert(false);
+//				return;
+//			}
+//			_s = new char[strlen(s) + 1];
+//			strcpy(_s, s);
+//		}
+//		~string(){
+//			if (_s){
+//				delete[] _s;
+//				_s = nullptr;
+//			}
+//		}
+//	private:
+//		char* _s;
+//	};
+//}
+//int main(){
+//	self::string k = "hello";
+//	self::string i("world");
+//	self::string m;
+//	//self::string l(k);
+//	return 0;
+//}
