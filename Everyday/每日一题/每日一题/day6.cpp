5440d8f60aacc1315975e2801bd04ca4d9e73aa4
sonissa <48381669+sonissa@users.noreply.github.com>
2019-11-14 23:30:32 +0800

    update

--- Everyday/每日一题/每日一题/day6.cpp
+++ Everyday/每日一题/每日一题/day6.cpp
@@ -1,20 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
+#include<vector>
 using namespace std;
+//int main()
+//{
+//	int w, h, res = 0;
+//	cin >> w >> h;
+//	vector<vector<int>> a;
+//	a.resize(w);
+//	for (auto& e : a)
+//		e.resize(h, 1);
+//
+//	for (int i = 0; i<w; i++)
+//	{
+//		for (int j = 0; j<h; j++)
+//		{
+//			if (a[i][j] == 1)
+//			{
+//				res++;
+//				// 标记不能放蛋糕的位置
+//				if ((i + 2)<w)
+//					a[i + 2][j] = 0;
+//
+//				if ((j + 2)<h)
+//					a[i][j + 2] = 0;
+//			}
+//		}
+//	}
+//	cout << res;
+//	return 0;
+//}
+//#include<string>
 //int main(){
-//	int h,m;
-//	cin >> h >> m;
-//	for (int i = 0; i < h; ++i){
-//		for (int n = 0; n < m; ++n){
-//			if (((i-n)*((i-1)-n)+()*()))
+//	int sum = 0;
+//	string s;
+//	cin >> s;
+//	for (int i = 0; i < s.length(); ++i){
+//		if (s[i] == '-'){
+//
 //		}
 //	}
 //	return 0;
-//}
-
-//using namespace std;
-//int main(){
-//	unsigned a:4;
-//	cout << sizeof(a);
-//		 return 0;
 //}
---
+++ Everyday/每日一题/每日一题/day8.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+//int main(){
+//	int a, b,i=1;
+//	cin >> a >> b;
+//	for (int i = 1;; i++){
+//		if (i%a == 0){
+//			if (i%b == 0){
+//				cout << i;
+//				return 0;
+//			}
+//		}
+//		if (i%b == 0){
+//			if (i%a == 0){
+//				cout << i;
+//				return 0;
+//			}
+//		}
+//	}
+//	cout << i;
+//	return 0;
+//}
+#include<string>
+int main(){
+	int n, m = 0, res1 = 0,res2=0;
+	string s[100];
+	cin >> n;
+	for (int i = 0; i < n; i++){
+		cin >> s[i];
+	}
+	while (m < n){
+		if (s[m][1] - s[m][0] == 1){
+			res1++;
+		}
+		if (s[m][])
+	}
+	return 0;
+}
