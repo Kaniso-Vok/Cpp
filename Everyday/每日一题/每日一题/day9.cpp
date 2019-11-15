84ccd3633b2ce32b630fbe512af0b8969fb4468e
sonissa <48381669+sonissa@users.noreply.github.com>
2019-11-15 23:43:51 +0800

    update

---
+++ Everyday/每日一题/每日一题/day9.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//class UnusualAdd {
+//public:
+//	int addAB(int A, int B) {
+//		int temp = 0;
+//		while (B != 0){
+//			temp = A&B;
+//			A = A^B;
+//			B = temp << 1;
+//		}
+//		return A;
+//	}
+//};
