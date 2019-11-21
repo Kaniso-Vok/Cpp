072d5fd3ca51f72c38fe5655a7d3432241492b8c
sonissa <48381669+sonissa@users.noreply.github.com>
2019-11-21 23:10:14 +0800

    update

---
+++ Everyday/每日一题linux/day14.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+//int main() {
+//    int sum=0,month,day,year;
+//    int a[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+//    cin>>year>>month>>day;
+//    if(year%400==0||(year%4==0&&year%100!=0)){
+//      a[1]=29;
+//    }
+//    for(int i=0;i<month-1;i++){
+//      sum+=a[i];
+//    }
+//    cout<<sum+day;
+//    return 0;
+//}
+int main() {
+    
+    return 0;
+}
