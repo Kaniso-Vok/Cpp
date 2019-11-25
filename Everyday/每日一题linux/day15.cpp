8a83c91271e1543fd672a69e276edc60f485d615
sonissa <48381669+sonissa@users.noreply.github.com>
2019-11-25 22:13:44 +0800

    update

---
+++ Everyday/每日一题linux/day15.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int findNumberOf1(int num){
+//    int sum=0;
+//    while(num){
+//        if(1&num){
+//            sum++;
+//        }
+//        num=num>>1;
+//    }
+//    return sum;
+//}
+//int main(){
+//    int n;
+//    while(cin>>n){
+//    cout<<findNumberOf1(n)<<endl;
+//}
+//    return 0;
+//}
+
+int main() {
+  
+  return 0;
+}
+
