b0eb6369a908b15db88dc8f291b8d7ead4ab5030
sonissa <48381669+sonissa@users.noreply.github.com>
2019-12-14 23:52:56 +0800

    update

--- Everyday/每日一题/每日一题/day13.cpp
+++ Everyday/每日一题/每日一题/day13.cpp
@@ -5,11 +5,32 @@
 //using namespace std;
 //
 //int main(){
-//	vector<string> s;
-//	while (getchar() != ' '){
-//		if (getchar() == '"'){
-//			
+//	string str,stt;
+//	int i=0;
+//	bool flag = 0;
+//	vector<string> arr;
+//	while (getline(cin, str)){
+//		for (int i = 0; i < str.size(); ++i){
+//			if (str[i] == '"'){
+//				flag = ~flag;
+//			}
+//			else{
+//				if (str[i] == ' ' && flag==0){
+//					arr.push_back(stt);
+//					stt = "";
+//				}
+//				else{
+//					if (str[i]!='"')
+//					stt += str[i];
+//				}
+//			}
+//		}
+//		arr.push_back(stt);
+//		cout << arr.size() << endl;
+//		for (int i = 0; i < arr.size(); i++){
+//			cout << arr[i] << endl;
 //		}
 //	}
 //	return 0;
 //}
+
