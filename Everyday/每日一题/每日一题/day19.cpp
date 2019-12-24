d6515f8b299188e4d35b9cb92ff6b9169db24887
sonissa <48381669+sonissa@users.noreply.github.com>
2019-12-24 21:22:36 +0800

    update

--- Everyday/每日一题/每日一题/day19.cpp
+++ Everyday/每日一题/每日一题/day19.cpp
@@ -1,6 +1,38 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
 using namespace std;
+
+int main(){
+	string str1, str2;
+	while (cin >> str1 >> str2){
+		if (str1.size() > str2.size()){
+			swap(str1, str2);
+		}
+		int le1 = str1.size(), le2 = str2.size(),max=0,sta=0;
+		vector<vector<int>> str(le1 + 1, vector<int>(le2 + 1, 0));
+		for (int i = 1; i <= le1; ++i){
+			for (int j = 1; j <= le2; ++j){
+				if (str1[i-1] == str2[j-1]){
+					str[i][j] = str[i - 1][j - 1] + 1;
+				}
+				if (str[i][j]>max){
+					max = str[i][j];
+					sta = i - max;
+				}
+			}
+		}
+		cout << str1.substr(sta,max) << endl;    
+	}
+	return 0;
+}
+
+
+
+
+
 /*
 递归问题
 3个瓶子换1瓶水+1个空瓶子，两个瓶子换1瓶水+0个空瓶子，1个瓶子换0瓶水。
@@ -32,3 +64,4 @@ f(n) = f(n-2)+1 */
 //	}
 //	return 0;
 //}
+
