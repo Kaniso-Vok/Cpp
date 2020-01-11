36f314a79fb998a47de666d4a3a68c4f3ac556c0
sonissa <48381669+sonissa@users.noreply.github.com>
2020-01-11 11:39:50 +0800

    day1

---
+++ vacation/future/future/day1.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+	void rotate(vector<int>& nums, int k) {
+		int sum = 0, s = nums.size();
+		int i = s - k%s;
+		if (s <= 1){
+			return;
+		}
+		vector<int> w;
+		while (sum < s){
+			if (i > s-1){
+				i = i-s;
+			}
+			w.push_back(nums[i]);
+			i++;
+			sum++;
+		}
+		nums = w;
+	}
+};
+int main(){
+	Solution L;
+	vector<int> k = { 1, 2,3,4,5,6,7};
+	L.Solution::rotate(k, 3);
+	for (auto e : k){
+		cout << e << ' ';
+	}
+	return 0;
+}
