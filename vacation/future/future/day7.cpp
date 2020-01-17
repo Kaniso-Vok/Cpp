85a38d7b2bf7212ff61abaa9791a1e5cbf71a5a3
sonissa <48381669+sonissa@users.noreply.github.com>
2020-01-17 21:19:10 +0800

    update

--- vacation/future/future/day7.cpp
+++ vacation/future/future/day7.cpp
@@ -3,15 +3,46 @@
 #include <vector>
 using namespace std;
 
-vector<int> sortArrayByParity(vector<int>& A) {
+//int pivotindex(vector<int>& nums) {
+//	int sum = 0;
+//	for (int item : nums)
+//		sum += item;
+//	cout << "sum = " << sum << endl;
+//	int sum_left = 0;
+//	for (int i = 0; i<nums.size(); i++) {
+//		if (sum_left == sum - sum_left - nums[i])
+//			return i;
+//		sum_left += nums[i];
+//	}
+//	return -1;
+//}
 
-}
-
-int main()
-{
-	
-	return 0;
-}
+//vector<int> sortArrayByParity(vector<int>& A) {
+//	vector<int> w;
+//	for (int e : A){
+//		if (e % 2 == 0 && e != 0){
+//			w.push_back(e);
+//		}
+//	}
+//	for (int e : A){
+//		if (e %== 0){
+//			w.push_back(e);
+//		}
+//	}
+//	for (int e : A){
+//		if (e % 2 != 0){
+//			w.push_back(e);
+//		}
+//	}
+//	return w;
+//}
+//
+//int main()
+//{
+//	vector<int> w = { 1,0 };
+//	sortArrayByParity(w);
+//	return 0;
+//}
 
 //int main(){
 //	char *p = "abcdefgh", *r;
--- vacation/future/future/day8.cpp
+++ vacation/future/future/day8.cpp
@@ -1,1 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+vector<int> plusOne(vector<int>& digits) {
+
+}
+
+int main(){
+	vector<int> w = { 6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3 };
+	plusOne(w);
+	return 0;
+}
