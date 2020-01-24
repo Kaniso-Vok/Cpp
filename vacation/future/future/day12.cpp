95f9a723f9db103c394bb3600ad15b0c96712459
sonissa <48381669+sonissa@users.noreply.github.com>
2020-01-24 22:49:57 +0800

    update

--- vacation/future/future/day12.cpp
+++ vacation/future/future/day12.cpp
@@ -1,5 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+int findUnsortedSubarray(vector<int>& nums) {
+	vector<int>w = nums;
+	sort(begin(w), end(w));
+	int i = 0, j = w.size()-1;
+	for (int v : w){
+		if (v != nums[i]){
+			break;
+		}
+		i++;
+	}
+	if (i-1 == j){
+		return 0;
+	}
+	for (; j > 0; j--){
+		if (w[j] != nums[j]){
+			break;
+		}
+	}
+	return j-i+1;
+}
 
+int main(){
+	vector<int> w = { 1,2,3,4 };
+	findUnsortedSubarray(w);
+	return 0;
+}
