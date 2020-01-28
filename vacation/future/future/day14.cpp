b8adc0aa7d3fc19449d4161fadee42053b91b460
sonissa <48381669+sonissa@users.noreply.github.com>
2020-01-28 22:45:09 +0800

    update

--- vacation/future/future/day14.cpp
+++ vacation/future/future/day14.cpp
@@ -11,8 +11,17 @@ struct TreeNode {
 };
 
 vector<int> rightSideView(TreeNode* root) {
-
+	vector<int> res;
+	helper(root, 0, res);
+	return res;
 }
+void helper(TreeNode* root, int level, vector<int>& res){
+	if (!root) return;
+	if (res.size() == level) res.push_back(root->val);
+	helper(root->right, level + 1, res);
+	helper(root->left, level + 1, res);
+}
+
 int main(){
 
 	return 0;
